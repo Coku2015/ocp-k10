2726559cdd1128672e95604cd534da375bf8e685
Coku2015 <Coku2015@gmail.com>
2022-10-28 20:09:01 +0800

    modified:   ocp-aws-k10/k10-lei-deploy.sh

--- ocp-aws-k10/k10-lei-deploy.sh
+++ ocp-aws-k10/k10-lei-deploy.sh
@@ -83,7 +83,6 @@ randstr(){
 	index=0
 	strRandomPass=""
 	for i in {a..z}; do arr[index]=$i; index=`expr ${index} + 1`; done
-	for i in {A..Z}; do arr[index]=$i; index=`expr ${index} + 1`; done
 	for i in {0..9}; do arr[index]=$i; index=`expr ${index} + 1`; done
 	for i in {1..4}; do strRandomPass="$strRandomPass${arr[$RANDOM%$index]}"; done
 	echo $strRandomPass
